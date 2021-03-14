e765feb32e7893980090a2dfab61dc1de33b30d5
Harsha Srikara <harshasrikara@gmail.com>
2021-03-14 11:48:34 -0500

    revisions

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['TypeScript', 'Firebase', 'React', 'Python', 'Node.js', 'Kubernetes'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -134,22 +134,14 @@ const About = () => {
       <div className="inner">
         <StyledText>
           <div>
-            <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
-            </p>
+            <p>Hello! I'm Harsha, nice to meet you!</p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              I am an undergraduate student at the University of Texas at Dallas currently double
+              majoring in Computer Science and Mathematics. My interests primarily lie in the
+              confluence of software engineering and statistical analysis. This has led me to build
+              a strong background in the conceptual underpinnings of important fields such as Data
+              Science, Artificial Intelligence, Information Retrieval and Machine Learning.
             </p>
 
             <p>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -63,17 +63,11 @@ const Hero = () => {
   const two = <h2 className="big-heading">Harsha Srikara.</h2>;
   const three = <h3 className="big-heading">Hacker. Developer. Innovator.</h3>;
   const four = (
-    <>
-      <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
-      </p>
-    </>
+    <p>
+      I'm a Computer Science major at the University of Texas at Dallas, software developer at
+      Reveal-Brainspace and the Director of Development on the board for the Association for
+      Computing Machinery.
+    </p>
   );
   const five = (
     <a
