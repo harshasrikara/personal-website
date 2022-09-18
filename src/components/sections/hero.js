4f2d83ccbbd14a0d02115d78789e223cd71bf446
Harsha Srikara <harshasrikara@gmail.com>
2022-09-18 17:32:39 -0500

    remove incoming word

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -50,9 +50,7 @@ const Hero = () => {
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Harsha Srikara.</h2>;
   const three = <h3 className="big-heading">Hacker. Developer. Innovator.</h3>;
-  const four = (
-    <p>I'm an incoming Software Development Engineer at Amazon based out of Austin, TX.</p>
-  );
+  const four = <p>I'm a Software Development Engineer at Amazon based out of Austin, TX.</p>;
   const five = (
     <a href={`mailto:${email}`} className="email-link">
       Get In Touch
